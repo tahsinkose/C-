14008f08f589ca18579ad123da4ba4755591f683
Tahsincan Köse <tahsinkose@yandex.com>
2017-12-28 11:00:31 +0300

    Add explanatory comments.

--- visitCities.cpp
+++ visitCities.cpp
@@ -2,6 +2,12 @@
 #include "StackL.h"
 #include <cstdlib>
 
+/* Imitating Recursion through Stacks. 
+   It is known that Iterative Algorithms are much faster than their Recursion equivalents. Because each new function call
+   introduces a bunch of new state variables to execution stack. Even native recursion implementations can be converted into 
+   iterative equivalents through Stacks. This program is a small example of it.
+*/
+
 using namespace std;
 
 enum city {P=0,Q,R,S,T,W,X,Y,Z};
@@ -22,13 +28,13 @@ int main()
 			   0,0,0,0,0,0,0,0,1};
 
 	int visited[9]={0,0,0,0,0,0,0,0,0};
-
+       // Starting city is T:
        myStack.push(T);
        visited[T]=1;
-
+	// Trying to reach Z.
        while ((!myStack.isEmpty()) && (myStack.getTop()!=Z))
        {
-       	       city tmp=myStack.getTop(); myStack.showContent();	 	 
+       	       city tmp=myStack.getTop(); //myStack.showContent();	 	 
 	       bool backtrackflag=true; 	
                for (int c=0; c<=8; c++)
 	          if ((m[tmp][c]== 1) && (visited[c]==0)) {
@@ -41,7 +47,7 @@ int main()
 	if (myStack.isEmpty()) cout<<"No path exists"<<endl;
 	else cout<<"Path Exists"<<endl;
         
-        myStack.showContent();	
+        //myStack.showContent();	
 
 	return 0;
 
